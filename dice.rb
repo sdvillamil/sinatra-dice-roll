2ce25133ec536784d6f1529cf3c6138989126bc9
sdvillamil <156153167+sdvillamil@users.noreply.github.com>
2024-10-22 09:28:06 +0000

    dd

--- dice.rb
+++ dice.rb
@@ -1,5 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+
+
 
 get("/") do
   '<p>Hello World</p>
