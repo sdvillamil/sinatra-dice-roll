9e2b421695a4c1889344d58e69deed206491ddd7
sdvillamil <156153167+sdvillamil@users.noreply.github.com>
2024-10-22 11:53:55 +0000

    ff

--- dice.rb
+++ dice.rb
@@ -18,42 +18,59 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die =rand(1..10)
+    
+    @rolls.push(die)
+  end
 
-  erb(:two_six, { :layout => :wrapper })
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
-	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  2.times do
+    die =rand(1..10)
+    
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
 end
 get("/dice/1/20") do
-  die = rand(1..20)
-	
-	outcome = "You rolled a #{die}."
-	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  1.times do
+    die =rand(1..20)
+    
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  5.times do
+    die =rand(1..4)
+    
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die =rand(1..6)
+    
+    @rolls.push(die)
+  end
+
+  erb(:onehundred_six)
 end
